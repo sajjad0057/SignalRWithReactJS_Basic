5504a7fa37108cf864ade4d6108b101996d6b21c
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-02-28 12:34:38 +0600

    create 1st ChatHub (simple to understand)

---
+++ signalR_backend/SimpleChat.Api/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using SimpleChat.Api.Models;
+
+namespace SimpleChat.Api.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task JoinChat(UserConnection connect)
+    {
+        await Clients.All
+            .SendAsync("ReceiveMessage", "admin", $"{connect.UserName} has joined");
+    }
+}
